dde61eed5c3274e6422bbfb5238f142a6ec84c9f
Edgar-Paina <ed01041994@gmail.com>
2020-09-29 18:42:05 -0500

    Casi terminado T.T

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireModule} from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { SendEmailComponent } from './auth/send-email/send-email.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SendEmailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ReactiveFormsModule,AngularFireModule.initializeApp(environment.firebaseConfig)
+    ,AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDiexr5OH_0h_u85-CbjiJUwyP360unvv8",
+    authDomain: "login-ng-45713.firebaseapp.com",
+    databaseURL: "https://login-ng-45713.firebaseio.com",
+    projectId: "login-ng-45713",
+    storageBucket: "login-ng-45713.appspot.com",
+    messagingSenderId: "548069663338",
+    appId: "1:548069663338:web:0482b5946467fe5c5de013"
+  },
 };
 
 /*
