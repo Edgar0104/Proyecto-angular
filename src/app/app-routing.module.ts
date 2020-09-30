0b9687933a6d55341ea3e3270b46c25d6c85770d
Edgar-Paina <ed01041994@gmail.com>
2020-09-30 18:50:16 -0500

    tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { SendEmailComponent } from './auth/send-email/send-email.component';
 const routes: Routes = [
   {
     path:'',
-    redirectTo:'/home',
+    redirectTo:'/login',
     pathMatch:'full',
   },
   
-  { path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)},
   { path: 'login', loadChildren: () => import('./auth/login/login.module').then((m) => m.LoginModule)},
   { path: 'register', loadChildren: () => import('./auth/register/register.module').then((m) => m.RegisterModule)},
   { path: 'verification-email', component:SendEmailComponent,},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -9,6 +9,7 @@ import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFireModule} from '@angular/fire';
 import { environment } from 'src/environments/environment';
 import { SendEmailComponent } from './auth/send-email/send-email.component';
+
 
 
 @NgModule({
